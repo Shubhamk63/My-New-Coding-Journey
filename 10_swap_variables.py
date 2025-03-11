f77bba2c2c6adc06ebbc105e3a558526c07dcdfb
shubham <shubham301@flash.co>
2025-03-11 20:07:48 +0530

    changes

--- 10_swap_variables.py
+++ 10_swap_variables.py
@@ -3,4 +3,4 @@ b = 12
 
 a,b = b,a
 print(f"the value of a is: {a} and value of b is {b}") 
-# Output: the value of a is: 12 and value of b is 11b
+# Output: the value of a is: 12 and value of b is 11b is 11
