5b0d8dd48d1934d8079fff90a25bc8bc0baf26ec
shubham <shubham301@flash.co>
2025-03-11 20:12:53 +0530

    Fix output comment in swap_variables.py and add newline at end of file

--- 10_swap_variables.py
+++ 10_swap_variables.py
@@ -3,4 +3,5 @@ b = 12
 
 a,b = b,a
 print(f"the value of a is: {a} and value of b is {b}") 
-# Output: the value of a is: 12 and value of b is 11b is 11
+# Output: the value of a is: 12 and value of b is 11b is 11  
+#new file was added to the repository
